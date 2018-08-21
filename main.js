f16f95a774150b1d79369b401b63f4c81c5ee5df
Greg Omer <gregoryeomer@live.com>
2018-08-20 23:26:16 -0600

    major concepts functional

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+console.log("readddds")
+let curColor = 'red'
+
+document.getElementById("wrapper").addEventListener("click", function(event) {
+  event.target.style.background = curColor
+});
+document.getElementById("colors").addEventListener("click", function(event) {
+  curColor = event.target.style.background
+  console.log(curColor)
+});
+document.getElementsByClassName("redPaint")[0].addEventListener("click", function(event) {
+
+
+
+console.log(event.target.classList[0].background)
+
+});
+
+
+
+
+
+});
