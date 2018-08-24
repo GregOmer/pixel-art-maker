aa31372f7c42d882e80d53c0b33e6289aba4115b
Greg Omer <gregoryeomer@live.com>
2018-08-24 15:44:45 -0600

    changes to current color, grid, added loop generated canvas.

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 console.log("readddds")
 
-let curColor = 'red'
+let curColor = 'purple'
 
-document.getElementById("wrapper").addEventListener("click", function(event) {
+
+document.getElementById('wrapper').addEventListener('click', function(event){
   event.target.style.background = curColor
-});
-document.getElementById("colors").addEventListener("click", function(event) {
+})
+
+document.getElementById("colors").addEventListener("click", function(event){
   curColor = event.target.style.background
-  console.log(curColor)
-});
-document.getElementsByClassName("redPaint")[0].addEventListener("click", function(event) {
-
-
-
-console.log(event.target.classList[0].background)
+  document.getElementById('body').style.background = curColor
 
 });
+let wrapper = document.getElementById('wrapper');
+
+for(let i = 0; i < 512; i++){
+  let loopDiv = document.createElement('div');
+  loopDiv.className = 'pixel';
+  wrapper.appendChild(loopDiv);
+}
+
+
+
+
+
+
+
 });
