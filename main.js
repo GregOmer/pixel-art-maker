4967d89b75b272565910f61bafe69f808b570d91
Greg Omer <gregoryeomer@live.com>
2018-08-25 21:04:03 -0600

    ditched switches board

--- main.js
+++ main.js
@@ -3,33 +3,43 @@ console.log("readddds")
 
 let curColor = 'purple'
 
-let pixels = document.getElementById('pixel')
 
-let canvColo = document.getElementById("wrapper").style.background;
+for(let i = 0; i < 512; i++){
+  let loopDiv = document.createElement("div");
+  loopDiv.Id = "pixel";
+  loopDiv.style.background = 'white';
+  wrapper.appendChild(loopDiv);
+};
 
-console.log()
+console.log(document.getElementById('pixel'))
+
+
+let pixels = document.getElementById('pixel');
+
+// let canvasBackColor = pixels.style.background;
 
 document.getElementById("wrapper").addEventListener("click", function(event){
   event.target.style.background = curColor
 })
+// console.log(document.getElementById("kghkjsdbg", clearButton))
 
 // clear function---NONFUNCTIONING!!
-// document.getElementById("clearButton").addEventListener('click', function(event){
-//   canvColo = white
-// })
+document.getElementById("clearButton").addEventListener('click', function(event){
+  canvasBackColor = 'white'
+})
 
 
 document.getElementById("colors").addEventListener("click", function(event){
   curColor = event.target.style.background
   document.getElementById("body").style.background = curColor
 
-});
+})
+document.getElementById('blackButton').addEventListener('click', function(event){
+  console.log("blackButton clicked")
+  document.getElementsByTagName('loopDiv').style.background = 'black';
 
-for(let i = 0; i < 512; i++){
-  let loopDiv = document.createElement("div");
-  loopDiv.Id = "pixel";
-  wrapper.appendChild(loopDiv);
-}
+})
 
 
-});
+
+})
